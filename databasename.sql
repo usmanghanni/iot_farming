c6cd1703ee697b2ced711cbdda6a3bbcd6ee3d66
usmanghanni <maniusman943@gmail.com>
2021-03-17 20:32:41 +0500

    db schema added

---
+++ databasename.sql
@@ -0,0 +1,63 @@
+-- MariaDB dump 10.17  Distrib 10.4.13-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: kkkp
+-- ------------------------------------------------------
+-- Server version	10.4.13-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `kkkp`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `kkkp` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `kkkp`;
+
+--
+-- Table structure for table `sensdata`
+--
+
+DROP TABLE IF EXISTS `sensdata`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sensdata` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `temp1` int(11) NOT NULL,
+  `temp2` int(11) NOT NULL,
+  `hum1` int(11) NOT NULL,
+  `hum2` int(11) NOT NULL,
+  `time_added` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`sid`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sensdata`
+--
+
+LOCK TABLES `sensdata` WRITE;
+/*!40000 ALTER TABLE `sensdata` DISABLE KEYS */;
+INSERT INTO `sensdata` VALUES (1,25,24,60,61,'2021-03-17 12:17:33'),(2,31,30,56,55,'2021-03-17 12:18:34'),(3,21,20,52,50,'2021-03-17 12:18:45'),(4,12,15,55,56,'2021-03-17 12:26:19'),(5,33,32,51,56,'2021-03-17 14:47:46'),(6,36,33,54,58,'2021-03-17 14:48:06'),(7,37,35,51,52,'2021-03-17 14:48:36'),(8,39,37,50,51,'2021-03-17 14:49:01'),(9,36,39,56,50,'2021-03-17 14:49:14'),(10,32,32,50,58,'2021-03-17 14:49:35'),(11,44,41,59,61,'2021-03-17 14:49:51'),(12,48,44,52,64,'2021-03-17 14:50:12');
+/*!40000 ALTER TABLE `sensdata` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-17 20:25:35
